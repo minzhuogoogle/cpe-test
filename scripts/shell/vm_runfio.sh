3e1ebeaf78c336cd950d846252eb769d9f9191ec
Min Zhuo <mzhuo@google.com>
2018-10-13 13:59:08 -0700

    Update vm_runfio.sh

--- scripts/shell/vm_runfio.sh
+++ scripts/shell/vm_runfio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt install fio -y
-sudo apt install nfs-common -y
+#sudo apt-get update
+#sudo apt install fio -y
+#sudo apt install nfs-common -y
 sudo mkdir -p /mnt/elastifile
 sudo mount -o nolock 10.99.0.2:/DC01/root /mnt/elastifile
 cd /mnt/elastifile
@@ -10,10 +10,6 @@ gcloud auth activate-service-account --key-file elastifile.json
 sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/fio/elastifile/fio.profile
 NOW=$(date +"%Y.%m.%d")
 HOSTNAME=$(hostname)
-sudo fio fio.profile  --refill_buffers --norandommap --time_based  --group_reporting  --output elastifile.$HOSTNAME.$NOW.log
+sudo fio fio.profile  --refill_buffers --norandommap --time_based --output-format=json --output elastifile.$HOSTNAME.$NOW.log
 gsutil cp elastifile.$HOSTNAME.$NOW.log gs://cpe-performance-storage/test_result/elastifile.$HOSTNAME.$NOW.log
 sudo rm *.*
-project='cpe-performance-storage'
-zone='us-east1-b'
-
-#for i in `gcloud compute instances list --project $project --filter='test-elastifile-storage' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
