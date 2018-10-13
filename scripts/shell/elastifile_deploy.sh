a20140888215bf0b0b0d281f280c8bd1a1ec3b8a
Min Zhuo <mzhuo@google.com>
2018-10-13 01:29:21 -0700

    Update elastifile_deploy.sh

--- scripts/shell/elastifile_deploy.sh
+++ scripts/shell/elastifile_deploy.sh
@@ -7,12 +7,12 @@ zone='us-east1-b'
 for i in `gcloud compute instances list --project $project --filter='test-elastifile-storage' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
 terraform init
 terraform  apply --auto-approve
-NOW=$(date +"%m.%d.%Y")
+NOW=$(date +"%Y%m%d")
 HOSTNAME=$(hostname)
+instance_name=vm-$HOSTNAME-$NOW
 gsutil cp create_vheads.log gs://cpe-performance-storage/test_result/create_vheads.$HOSTNAME.$NOW.log
 
 project='cpe-performance-storage'
-instance_name=$HOSTNAME
 zone='us-east1-b'
 machine_type='n1-standard-4'
 gcloud beta compute --project=$project instances create $instance_name --zone=$zone --machine-type=$machine_type --metadata=startup-script=curl\ -OL\ https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/scripts/shell/vm_runfio.sh$'\n'sudo\ chmod\ 777\ vm_runfio.sh$'\n'sudo\ ./vm_runfio.sh --boot-disk-size=10GB
