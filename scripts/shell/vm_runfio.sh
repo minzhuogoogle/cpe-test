db1fb114db34a7e4a4a60909325f896844d457ca
Min Zhuo <mzhuo@google.com>
2018-10-13 15:30:02 -0700

    Update vm_runfio.sh

--- scripts/shell/vm_runfio.sh
+++ scripts/shell/vm_runfio.sh
@@ -5,11 +5,11 @@
 sudo mkdir -p /mnt/elastifile
 sudo mount -o nolock 10.99.0.2:/DC01/root /mnt/elastifile
 cd /mnt/elastifile
-sudo gsutil cp gs://cpe-performance-storage/cpe-performance-storage-b13c1a7348ad.json elastifile.json 
+gsutil cp gs://elastifile_test/elastifile.json elastifile.json 
 gcloud auth activate-service-account --key-file elastifile.json
 sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/fio/elastifile/fio.profile
 NOW=$(date +"%Y.%m.%d")
 HOSTNAME=$(hostname)
 sudo fio fio.profile  --refill_buffers --norandommap --time_based --output-format=json --output elastifile.$HOSTNAME.$NOW.log
-gsutil cp elastifile.$HOSTNAME.$NOW.log gs://cpe-performance-storage/test_result/elastifile.$HOSTNAME.$NOW.log
+gsutil cp elastifile.$HOSTNAME.$NOW.log gs://elastifile_test/test_result/elastifile.$HOSTNAME.$NOW.log
 sudo rm *.*
