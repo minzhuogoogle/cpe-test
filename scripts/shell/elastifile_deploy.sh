477d18e19f525ddd4de7df37d5fc87777030edbb
Min Zhuo <mzhuo@google.com>
2018-10-13 14:18:24 -0700

    Update elastifile_deploy.sh

--- scripts/shell/elastifile_deploy.sh
+++ scripts/shell/elastifile_deploy.sh
@@ -1,3 +1,4 @@
+date
 cd Terraform-Elastifile-GCP/ 
 gsutil cp gs://cpe-performance-storage/cpe-performance-storage-b13c1a7348ad.json elastifile.json 
 gcloud auth activate-service-account --key-file elastifile.json
@@ -6,8 +7,10 @@ zone='us-east1-b'
 
 for i in `gcloud compute instances list --project $project --filter='evm-' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
 for i in `gcloud compute instances list --project $project --filter='test-elastifile-storage' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
+date
 terraform init
 terraform  apply --auto-approve
+date
 NOW=$(date +"%Y%m%d")
 HOSTNAME=$(hostname)
 instance_name=evm-$HOSTNAME-$NOW
