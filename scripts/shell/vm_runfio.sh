35fa914d89cf3fcd15aab4f3552bc5acee99b06f
Min Zhuo <mzhuo@google.com>
2018-10-13 17:31:20 -0700

    Update vm_runfio.sh

--- scripts/shell/vm_runfio.sh
+++ scripts/shell/vm_runfio.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#sudo apt-get update
-#sudo apt install fio -y
-#sudo apt install nfs-common -y
+sudo apt-get update
+sudo apt install fio -y
+sudo apt install nfs-common -y
 sudo mkdir -p /mnt/elastifile
 sudo mount -o nolock 10.99.0.2:/DC01/root /mnt/elastifile
 cd /mnt/elastifile
-gsutil cp gs://elastifile_test/nelastifile.json elastifile.json 
+#gsutil cp gs://elastifile_test/nelastifile.json elastifile.json 
 #gcloud auth activate-service-account --key-file elastifile.json
 sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/fio/elastifile/fio.profile
 NOW=$(date +"%Y.%m.%d")
