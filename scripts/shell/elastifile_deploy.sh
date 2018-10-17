e56ea73e3cab46079780c9c7ed18ed035e4efb4e
Min Zhuo <mzhuo@google.com>
2018-10-16 17:55:48 -0700

    Update elastifile_deploy.sh

--- scripts/shell/elastifile_deploy.sh
+++ scripts/shell/elastifile_deploy.sh
@@ -1,7 +1,8 @@
 date
-#rm -rf Terraform-Elastifile-GCP
+rm -rf Terraform-Elastifile-GCP
 #git clone https://github.com/minzhuogoogle/Terraform-Elastifile-GCP.git
-#echo sunnydog > Terraform-Elastifile-GCP/password.txt
+git clone https://github.com/Elastifile/gcp-automation.git
+echo sunnydog > Terraform-Elastifile-GCP/password.txt
 cd Terraform-Elastifile-GCP/ 
 #gsutil cp gs://elastifile_test/nelastifile.json elastifile.json
 gsutil cp gs://cpe-performance-storage/cpe-performance-storage-b13c1a7348ad.json elastifile.json
@@ -15,9 +16,10 @@ zone='us-east1-b'
 for i in `gcloud compute instances list --project $project --filter='evm-' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
 #for i in `gcloud compute instances list --project $project --filter='test-elastifile-storage' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
 date
-#cd Terraform-Elastifile-GCP/ 
-#terraform init
-#terraform  apply --auto-approve
+cd Terraform-Elastifile-GCP/ 
+sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/elastifile/terraform.tfvars
+terraform init
+terraform  apply --auto-approve
 date
 NOW=$(date +"%Y%m%d")
 #NOW=`date +%m.%d.%Y.%H.%M.%S`
