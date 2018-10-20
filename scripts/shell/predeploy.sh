d221d6894d66c321a12eb07fcf00ed6d6710d187
Min Zhuo <mzhuo@google.com>
2018-10-19 22:11:38 -0700

    Update predeploy.sh

--- scripts/shell/predeploy.sh
+++ scripts/shell/predeploy.sh
@@ -1,14 +1,9 @@
 date
-rm -rf Terraform-Elastifile-GCP
-#git clone https://github.com/minzhuogoogle/Terraform-Elastifile-GCP.git
 git clone https://github.com/Elastifile/gcp-automation.git
 echo sunnydog > gcp-automation/password.txt
-cd gcp-automation/ 
-#gsutil cp gs://elastifile_test/nelastifile.json elastifile.json
+
 gsutil cp gs://cpe-performance-storage/cpe-performance-storage-b13c1a7348ad.json elastifile.json
 gcloud auth activate-service-account --key-file  elastifile.json
-#project='gtp-cpe-integration-testing'
-#zone='us-west1-b'
 
 HNOW=$(date +"%Y%m%d")
 NOW=`date +%m.%d.%Y.%H.%M.%S`
@@ -17,21 +12,20 @@ instance_name=evm-$HOSTNAME
 
 for i in `gcloud compute instances list --project $project --filter='evm-' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
 for i in `gcloud compute instances list --project $project --filter='lssd-elastifile-storage' | grep -v NAME | cut -d ' ' -f1`; do gcloud compute instances delete $i --project $project --zone $zone -q; done
+
 date
+cd gcp-automation/ 
 curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/elastifile/terraform.tfvars.local.ssd
 curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/elastifile/terraform.tfvars.pssd
 curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/elastifile/terraform.tfvars.phhd
 
-elfs_types=("terraform.tfvars.local.ssd"  "terraform.tfvars.pssd" "terraform.tfvars.phhd" )
-for i in "${elfs_types[@]}"; do   # The quotes are necessary here
- 
-   #echo "Bypassing elastifile provisioning due to fatal problem..........."   
+elfs_types=("terraform.tfvars.local.ssd" "terraform.tfvars.pssd" "terraform.tfvars.phhd" )
+for i in "${elfs_types[@]}"
+do   
+   echo "Start provision", $i   
    terraform init
-   terraform  apply --auto-approve -var-file $i
+   terraform apply -var-file $i --auto-approve   
    date
-
-#project='cpe-performance-storage'
-#zone='us-east1-b'
    export zone=`grep ZONE terraform.tfvars | awk -v N=3 '{print $N}'`
    export project=`grep PROJECT terraform.tfvars | awk -v N=3 '{print $N}'`
    export cluster_name=`grep CLUSTER_NAME terraform.tfvars | awk -v N=3 '{print $N}'`
@@ -40,10 +34,10 @@ for i in "${elfs_types[@]}"; do   # The quotes are necessary here
    HNOW=$(date +"%Y%m%d")
    NOW=`date +%m.%d.%Y.%H.%M.%S`
    HOSTNAME=$(hostname)
-   instance_name=disk-$HOSTNAME
+   instance_name=$disk-$HOSTNAME
+   echo $instance_name
    cat $i >> create_vheads.log 
    gsutil cp create_vheads.log gs://cpe-performance-storage/test_result/create_vheads.$HOSTNAME.$NOW.txt
    machine_type='n1-standard-4'
    gcloud compute --project=$project instances create $instance_name  --zone=$zone --machine-type=$machine_type --scopes=https://www.googleapis.com/auth/devstorage.read_write --metadata=startup-script=sudo\ curl\ -OL\ https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/scripts/shell/vm_runfio.sh\;\ sudo\ chmod\ 777\ vm_runfio.sh\;\ sudo\ ./vm_runfio.sh  
-
 done
