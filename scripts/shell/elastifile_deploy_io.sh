f9390d8a3dbfb3ce78c070da373ff114a6b9db1c
Min Zhuo <mzhuo@google.com>
2018-10-20 01:59:23 -0700

    Update elastifile_deploy_io.sh

--- scripts/shell/elastifile_deploy_io.sh
+++ scripts/shell/elastifile_deploy_io.sh
@@ -39,4 +39,4 @@ HOSTNAME=$(hostname)
 
 gsutil cp create_vheads.log gs://cpe-performance-storage/test_result/create_vheads.$disktype.$HOSTNAME.$NOW.txt
 machine_type='n1-standard-4'
-gcloud compute --project=$project instances create $instance_name  --zone=$zone --machine-type=$machine_type --scopes=https://www.googleapis.com/auth/devstorage.read_write --metadata=startup-script=sudo\ curl\ -OL\ https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/scripts/shell/vm_runfio.sh\;\ sudo\ chmod\ 777\ vm_runfio.sh\;\ sudo\ ./vm_runfio.sh  
+gcloud compute --project=$project instances create $instance_name  --zone=$zone --machine-type=$machine_type --scopes=https://www.googleapis.com/auth/devstorage.read_write --metadata=startup-script=sudo\ curl\ -OL\ https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/scripts/shell/vm_runfio.sh\;\ sudo\ chmod\ 777\ vm_runfio.sh\;\ sudo\ ./vm_runfio.sh $disk 
