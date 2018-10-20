28b67b8a81c4b673477d5f5a1676b145e4087b33
Min Zhuo <mzhuo@google.com>
2018-10-20 01:36:20 -0700

    Update elastifile_deploy_io.sh

--- scripts/shell/elastifile_deploy_io.sh
+++ scripts/shell/elastifile_deploy_io.sh
@@ -2,13 +2,13 @@ disktype=$1
 echo $disktype
 echo `date`
 cd gcp-automation/ 
-curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/elastifile/terraform.tfvars.lssd 
-curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/elastifile/terraform.tfvars.pssd
-curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/elastifile/terraform.tfvars.phdd 
+
 gsutil cp gs://cpe-performance-storage/cpe-performance-storage-b13c1a7348ad.json elastifile.json
 gcloud auth activate-service-account --key-file  elastifile.json
 
 cp terraform.tfvars.$disktype terraform.tfvars
+cat terraform.tfvars
+
 export zone=`grep ZONE terraform.tfvars | awk -v N=3 '{print $N}'`
 zone=${zone:1:-1}
 export project=`grep PROJECT terraform.tfvars | awk -v N=3 '{print $N}'`
@@ -40,4 +40,3 @@ HOSTNAME=$(hostname)
 gsutil cp create_vheads.log gs://cpe-performance-storage/test_result/create_vheads.$disktype.$HOSTNAME.$NOW.txt
 machine_type='n1-standard-4'
 gcloud compute --project=$project instances create $instance_name  --zone=$zone --machine-type=$machine_type --scopes=https://www.googleapis.com/auth/devstorage.read_write --metadata=startup-script=sudo\ curl\ -OL\ https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/scripts/shell/vm_runfio.sh\;\ sudo\ chmod\ 777\ vm_runfio.sh\;\ sudo\ ./vm_runfio.sh  
-
