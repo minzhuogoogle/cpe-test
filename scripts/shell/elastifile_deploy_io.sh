0355ceb35e96cdef46dd4c3ce299941f6f0c7b97
Min Zhuo <mzhuo@google.com>
2018-10-21 12:13:57 -0700

    Update elastifile_deploy_io.sh

--- scripts/shell/elastifile_deploy_io.sh
+++ scripts/shell/elastifile_deploy_io.sh
@@ -63,7 +63,7 @@ start_vm() {
  
      zone=$2
      disktype=$3
-     vm_name=$disktype-$hostname
+     vm_name=$disktype-$(hostname)
      machine_type='n1-standard-4'
      gcloud compute --project=$project instances create $vm_name  --zone=$zone --machine-type=$machine_type --scopes=https://www.googleapis.com/auth/devstorage.read_write --metadata=startup-script=sudo\ curl\ -OL\ https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/scripts/shell/vm_runfio.sh\;\ sudo\ chmod\ 777\ vm_runfio.sh\;\ sudo\ ./vm_runfio.sh\ $disktype  
      retval=$?
