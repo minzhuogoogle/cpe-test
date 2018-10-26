d3e70e1cdeb6bc66c5d0ef47f0cd2669a0f706d3
Min Zhuo <mzhuo@google.com>
2018-10-26 00:43:54 -0700

    Update vm_runfio.sh

--- scripts/shell/vm_runfio.sh
+++ scripts/shell/vm_runfio.sh
@@ -23,7 +23,7 @@ start_fio()
       #cd /mnt/elastifile
       echo $iotype $disktype $nfs_server
       sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/fio/elastifile/fio.$iotype
-      sudo sed -i 's/300/100/' fio.$iotype
+      sudo sed -i 's/300/600/' fio.$iotype
       NOW=`TZ=UTC+7 date +%m.%d.%Y.%H.%M.%S`
       HOSTNAME=$(hostname)
       logfile=elfs.fio.$iotype.$HOSTNAME.$NOW.$disktype.txt
