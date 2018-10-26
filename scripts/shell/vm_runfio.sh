35c9bb4664a63eb025dd80859f4b59a85d5e1c59
Min Zhuo <mzhuo@google.com>
2018-10-25 22:55:27 -0700

    Update vm_runfio.sh

--- scripts/shell/vm_runfio.sh
+++ scripts/shell/vm_runfio.sh
@@ -23,14 +23,14 @@ start_fio()
       cd /mnt/elastifile
       echo $iotype $disktype $nfs_server
       sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/fio/elastifile/fio.$iotype
-      sed -i 's/300/90/' fio.$iotype
+      sed -i 's/300/1200/' fio.$iotype
       NOW=`TZ=UTC+7 date +%m.%d.%Y.%H.%M.%S`
       HOSTNAME=$(hostname)
       logfile=elfs.fio.$iotype.$HOSTNAME.$NOW.$disktype.txt
       echo $logfile
       sudo fio fio.$iotype --refill_buffers --norandommap --time_based --output-format=json --output $logfile
       gsutil cp $logfile gs://cpe-performance-storage/test_result/$logfile
-      #sudo rm *.*.*
+      sudo rm *.*.*
 }
 
 disktypes=('lssd-elfs' 'pssd-elfs' 'phdd-elfs')
@@ -73,7 +73,7 @@ then
              start_fio $j $testtype $nfs_server
 	     export current=` date +"%s"`
 	     delta=$((current-fio_start))
-	     while [ $delta -lt < 120 ]
+	     while [ $delta -lt < 1300 ]
 	     do 
 	        sleep 1
 		export current=` date +"%s"`
