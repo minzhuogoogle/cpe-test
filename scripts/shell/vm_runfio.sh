88a1d5c477d4b321a92a41940dd4ff97a01d556a
Min Zhuo <mzhuo@google.com>
2018-10-26 00:33:58 -0700

    Update vm_runfio.sh

--- scripts/shell/vm_runfio.sh
+++ scripts/shell/vm_runfio.sh
@@ -20,17 +20,17 @@ start_fio()
       iotype=$1
       disktype=$2
       nfs_server=$3
-      cd /mnt/elastifile
+      #cd /mnt/elastifile
       echo $iotype $disktype $nfs_server
       sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/cpe-test/master/fio/elastifile/fio.$iotype
-      sed -i 's/300/600/' fio.$iotype
+      sed -i 's/300/100/' fio.$iotype
       NOW=`TZ=UTC+7 date +%m.%d.%Y.%H.%M.%S`
       HOSTNAME=$(hostname)
       logfile=elfs.fio.$iotype.$HOSTNAME.$NOW.$disktype.txt
       echo $logfile
-      sudo fio fio.$iotype --refill_buffers --norandommap --time_based --output-format=json --output $logfile
+      sudo fio /mnt/elastifile/fio.$iotype --refill_buffers --norandommap --time_based --output-format=json --output $logfile
       gsutil cp $logfile gs://cpe-performance-storage/test_result/$logfile
-      sudo rm *.*.*
+      sudo rm -rf /mnt/elastifile/fio.*
 }
 
 disktypes=('lssd-elfs' 'pssd-elfs' 'phdd-elfs')
@@ -55,7 +55,7 @@ if [ $count -lt $ping_retry ]
 then
       echo "Start fio on Elastifile datacontainer."
       sudo mount -o nolock $nfs_server:/$nfs_data_container/root /mnt/elastifile
-      cd /mnt/elastifile
+      #cd /mnt/elastifile
       declare -a iotype=('readbw' 'readiops' 'writebw' 'writeiops' 'randrwbw' 'randrwiops')
       number=0
       export now=` date +"%s"`
