01dde9c0abb405ef527da84458e1fc096185a007
Min Zhuo <mzhuo@google.com>
2018-10-30 09:19:03 -0700

    Update elfse2e.sh

--- scripts/shell/elfse2e.sh
+++ scripts/shell/elfse2e.sh
@@ -260,9 +260,9 @@ case "$testname" in
     *-io-* ) echo "prepare io only test";iotest=1;mfio=1;;
     *-ps-* ) echo "prepare postsubmit sanity test"; pstest=1;mfio=0;skipprovision=0;deletion=1;;
     *-cleanup-* ) echo "prepare to cleanup all resources used by testing"; cleanup=1;;
-    *-demo-lssd-* ) echo "prepare to run io on demo instance";iotest=1;snodename="demo-lssd-vm";clients=9;demo_test=1;
-    *-demo-pssd-* ) echo "prepare to run io on demo instance";iotest=1;snodename="demo-pssd-vm";demo_test=1;
-    *-demo-phdd-* ) echo "prepare to run io on demo instance";iotest=1;snodename="demo-phhd-vm";demo_test=1;
+    *-demo-lssd-* ) echo "prepare to run io on demo lssd instance";iotest=1;snodename="demo-lssd-vm";clients=3;demo_test=1;
+    *-demo-pssd-* ) echo "prepare to run io on demo pssd instance";iotest=1;snodename="demo-pssd-vm";clients=3;demo_test=1;
+    *-demo-phdd-* ) echo "prepare to run io on demo phhd instance";iotest=1;snodename="demo-phhd-vm";clients=3;demo_test=1;
     * ) echo "Error...";;
 esac
 
@@ -351,6 +351,7 @@ for i in nfs_server_ips
 do 
     vhead_count=$((vhead_count+1))
 done    
+clients=$((clients*vhead_count))
 echo "nfs servers:" $nfs_server_ip
 
 # TODO: get number of enodes from nfs_server_ips
