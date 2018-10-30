19343cb9bec7c4680e98a65c46f3a44fe9f4b079
Min Zhuo <mzhuo@google.com>
2018-10-29 23:41:07 -0700

    Update elfse2e.sh

--- scripts/shell/elfse2e.sh
+++ scripts/shell/elfse2e.sh
@@ -243,12 +243,12 @@ iotest=0
 cleanup=0
 
 case "$testname" in
-    *-daily-e2e* ) echo "prepare daily e2e test";mfio=0;skipprovision=0;deletion=0;;
+    *-daily-e2e* ) echo "prepare daily e2e test";mfio=0;skipprovision=0;deletion=1;;
     *-perf-* ) echo "preppare perf test";skipprovision=1;iotest=1;mfio=1;;
     *-scalability-* ) echo "prepare scability test";clients=9;iotest=1;mfio=1;;
     *elfs-ha-* ) echo "prepare ha test";ha=1;iotest=1;mfio=1;;
     *-io-* ) echo "prepare io only test";iotest=1;mfio=1;;
-    *-ps-* ) echo "prepare postsubmit sanity test"; pstest=1;mfio=0;skipprovision=0;deletion=0;;
+    *-ps-* ) echo "prepare postsubmit sanity test"; pstest=1;mfio=0;skipprovision=0;deletion=1;;
     *-cleanup-* ) echo "prepare to cleanup all resources used by testing"; cleanup=1;;
     * ) echo "Error...";;
 esac
