383ab97a9d78d73a02df1c45dc2576d144a0fdda
Min Zhuo <mzhuo@google.com>
2018-11-01 21:26:27 -0700

    Update newelfs.sh

--- scripts/shell/newelfs.sh
+++ scripts/shell/newelfs.sh
@@ -246,7 +246,7 @@ start_vm() {
      test_name=$4
      echo "project = $project"
      echo "zone = $zone"
-     zone="us-east1-b"
+     #zone="us-east1-b"
      machine_type='n1-standard-1'
 
      echo $testvmname
