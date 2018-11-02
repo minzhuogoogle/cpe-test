18dd19e2f74bdacf93c8dbe33e6c45b61a79f33e
Min Zhuo <mzhuo@google.com>
2018-11-01 20:39:58 -0700

    Update elfse2e.sh

--- scripts/shell/elfse2e.sh
+++ scripts/shell/elfse2e.sh
@@ -166,7 +166,7 @@ provision_elastifile() {
         echo "set number of node"
         sed -i "s/test-${disktype}/ps-${disktype}/g" terraform.tfvars
     fi
-    if  [ $pstest -eq 1 ]; then
+    if  [ $hatest -eq 1 ]; then
         echo "set number of node"
         sed -i "s/test-${disktype}/ha-${disktype}/g" terraform.tfvars
     fi
