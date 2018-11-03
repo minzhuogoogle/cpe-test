de21ee74b3c57a30eedf4b25c29db6efab6e3f1d
Min Zhuo <mzhuo@google.com>
2018-11-02 23:03:31 -0700

    Update elfse2e.sh

--- scripts/shell/elfse2e.sh
+++ scripts/shell/elfse2e.sh
@@ -603,7 +603,7 @@ test_result() {
     io_data_done=0
     echo "expected_logfile:" $expected_logfile
     echo $no_of_logfiles "-ge" $expected_logfile 
-    #if [ $no_of_logfiles -ge $expected_logfile ]; then
+   
     if (($no_of_logfiles < $expected_logfile )); then
        io_data_done=0
     else
@@ -618,16 +618,14 @@ test_result() {
       no_of_logfiles=$?
       echo "no of logfiles : " $no_of_logfiles "expected logfile: " $expected_logfile
       if [ $no_of_logfiles -ge $expected_logfile ]; then
-      #if (($no_of_logfiles < $expected_logfile )); then
+     
          echo "set test done"
          io_data_done=1
       else
          echo "set test not done"
          io_data_done=0
       fi
-       #   echo "yes. we got file"
-       #   io_date_done=1
-      #fi 
+      
       count=$((count+1))
     done
 
@@ -692,8 +690,8 @@ if [ $retval -ne 0 ]; then
 fi
 
 
-#post_cleanup
-#retval=$?
-#if [ $retval -ne 0 ]; then
-#    exit -1
-#fi
+post_cleanup
+retval=$?
+if [ $retval -ne 0 ]; then
+    exit -1
+fi
